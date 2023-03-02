d5a91738ca5fea1440da4a61a510770a9260ae49
Son <son@khanhson.local>
2023-03-02 22:08:38 +0700

    fix

--- menu-admin/js.js
+++ menu-admin/js.js
@@ -1,11 +1,13 @@
 const items = document.querySelectorAll(".aside__body-item");
 
-
 items.forEach((item) => {
   item.addEventListener("click", function (event) {
     items.forEach((el) => {
       el.classList.remove("active");
+      item.classList.add("active");
+      console.log(event.target)
     });
-    event.target.classList.add("active");
   });
 });
+
+const menuSideBar = document.querySelector(".aside__body-items");
